e9aa6a671d4d1aab51df4df39078f3b6b57dc41a
ayushguptaz <ayushgupta8009695@gmail.com>
2025-01-20 20:12:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express();
+
+app.get('/user/signup',function (req,res){
+
+})
+
+app.get('/user/signin', function(req,res){
+
+})
+
+app.get('/user/purchases', function(req,res){
+
+})
+
+app.get('/courses', function(req, res){
+
+})
+
+app.post('course/purchase', function(req,res){
+    
+})
+
+app.listen(3000);
